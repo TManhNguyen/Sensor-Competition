e61655505b8e7af42b28e47a46a3c6ca24545ffc
Manh Nguyen <108793626+TManhNguyen@users.noreply.github.com>
2022-11-30 17:31:35 +0700

    Competition Round 3

---
+++ SensorsRound3_-_Top_Rack/SensorsRound3_-_Top_Rack.ino
@@ -0,0 +1,108 @@
+#define BLYNK_TEMPLATE_ID "TMPL2qt2fgnY"
+#define BLYNK_DEVICE_NAME "Competition Round 3 Team 1"
+#define BLYNK_AUTH_TOKEN "8-wAN95lGPAS3oUXRy33TcVriRzxxDbT"
+#define BLYNK_PRINT Serial
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+char auth[] = BLYNK_AUTH_TOKEN;
+char ssid[] = "Hotspotify";
+char pass[] = "huhuhuhu";
+
+
+int point = 0;
+int trial = 1;
+int trialPinArray[] = {V8,V7,V6};
+int A = 0;
+int B = 0;
+int C = 0;
+int ultrasonic = 0;
+int conditional = 0;
+int colour = 0;
+int AR = 0; 
+
+void setup()
+{
+  Serial.begin(115200);
+  Blynk.begin(auth, ssid, pass);
+}
+
+void loop()
+{
+  Blynk.run();
+}
+
+BLYNK_WRITE(V5)
+{
+  if(param.asInt() == 1){
+    A = 2;
+  }else{
+    A = 0;
+  }
+  Blynk.virtualWrite(trialPinArray[trialBonus], A + B + C + ultrasonic + conditional + colour + AR);
+}
+
+BLYNK_WRITE(V9)
+{
+  if(param.asInt() == 1){
+    B = 2;
+  }else{
+    B = 0;
+  }
+  Blynk.virtualWrite(trialPinArray[trialBonus], A + B + C + ultrasonic + conditional + colour + AR);
+}
+
+BLYNK_WRITE(V10)
+{
+  if(param.asInt() == 1){
+    C = 2;
+  }else {
+    C = 0;
+  }
+  Blynk.virtualWrite(trialPinArray[trialBonus], A + B + C + ultrasonic + conditional + colour + AR);
+}
+BLYNK_WRITE(V4)
+{
+  trial = param.asInt()
+}
+
+BLYNK_WRITE(V0)
+{
+  if(param.asInt() == 1){
+    ultrasonic = 4;
+  }else{
+    ultrasonic = 0;
+  }
+  Blynk.virtualWrite(trialPinArray[trialBonus], A + B + C + ultrasonic + conditional + colour + AR);
+}
+
+BLYNK_WRITE(V1)
+{
+  if(param.asInt() == 1){
+    conditional = 4;
+  }else{
+    conditional = 0;
+  }
+  Blynk.virtualWrite(trialPinArray[trialBonus], A + B + C + ultrasonic + conditional + colour + AR);
+}
+
+BLYNK_WRITE(V2)
+{
+  if(param.asInt() == 1){
+    colour = 4;
+  }else{
+    colour = 0;
+  }
+  Blynk.virtualWrite(trialPinArray[trialBonus], A + B + C + ultrasonic + conditional + colour + AR);
+}
+
+BLYNK_WRITE(V3)
+{
+  if(param.asInt() == 1){
+    AR = 2;
+  }else{
+    AR = 0; 
+  }
+  Blynk.virtualWrite(trialPinArray[trialBonus], A + B + C + ultrasonic + conditional + colour + AR);
+}
